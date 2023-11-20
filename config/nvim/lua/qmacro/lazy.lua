8d3ed7a6856857d01759e996569858691161ed15
DJ Adams <dj.adams@sap.com>
2023-11-20 10:18:20 +0000

    improve nvim-cmp

--- config/nvim/lua/qmacro/lazy.lua
+++ config/nvim/lua/qmacro/lazy.lua
@@ -44,11 +44,20 @@ require('lazy').setup({
         }
     },
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp',
-        dependencies = {
-            'L3MON4D3/LuaSnip'
-        }
-    },
+    -- Completion
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'hrsh7th/cmp-nvim-lua' },
+    { 'hrsh7th/nvim-cmp' },
+
+--    { 'L3MON4D3/LuaSnip' },
+--    { 'saadparwaiz1/cmp_luasnip' },
+
+    { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/vim-vsnip' },
+    { 'rafamadriz/friendly-snippets' },
+
+    { 'onsails/lspkind.nvim' },
 
 })
