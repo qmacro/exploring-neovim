44bcff2d37c7b0403d12555ebde1e98027bbe7a8
DJ Adams <dj.adams@sap.com>
2023-11-21 08:51:11 +0000

    add telescope

--- config/nvim/lua/qmacro/lazy.lua
+++ config/nvim/lua/qmacro/lazy.lua
@@ -47,6 +47,14 @@ require('lazy').setup({
         'mfussenegger/nvim-lint'
     },
 
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        }
+    },
 
     -- Completion
     { 'hrsh7th/cmp-buffer' },
